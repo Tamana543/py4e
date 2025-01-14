8732d49e05b4f8dc8106e65bf206ac7325a092fb
Tamana543 <tamanafarzami33@gmail.com>
2025-01-14 08:34:30 +0430

    USA Elevator App Added

--- first.py
+++ first.py
@@ -1,15 +1,29 @@
-x = 3
-if x > 4:
-     print("bigger")
-if x < 4:
-     print("smaller")
+# x = 3
+# if x > 4:
+#      print("bigger")
+# if x < 4:
+#      print("smaller")
 
-n =5
-while n > 0:
-     print(n)
-     n = n-1
-print("completed")
+# n =5
+# while n > 0:
+#      print(n)
+     # n = n-1
+# print("completed")
 
-#Kind a adding a prompt 
-name = input("Who am I talking with ?")
-print("Hello",name)
+#Kind of adding a prompt 
+# name = input("Who am I talking with ?")
+# print("Hello",name)
+
+# Elevator System US to Europ convetor 
+floor  =  input("Which Floor? ")
+output = int(floor) + 1
+name = input("Enter your name ! ")
+
+# print(output , name ,  "Okay")
+
+# Assignment : prompt the user for hours and rate per hour using input to compute gross pay.
+hrs = input("Enter Hours: ")
+rate = input("Rate per hour ?")
+mainHrs = float(hrs)
+mainRate = float(rate)
+print("Pay:" , mainHrs * mainRate )
