f77296fe7b59a875b6976b8c6f3e72662cc68ebd
Tamana543 <tamanafarzami33@gmail.com>
2025-02-01 09:35:03 +0430

    File Updated

--- second.py
+++ second.py
@@ -1,12 +1,35 @@
-fname = input("Enter file name: ")
-data = open(fname)
-listData= list()
-for item in data:
-     items = item.strip().split()
-     for item in items: 
-          if item in listData :
-               continue
-          elif item  not in listData:
-               listData.append(item)
-listData.sort()
-print(listData)
+def Assignment84() :
+     fname = input("Enter file name: ")
+     data = open(fname)
+     listData= list()
+     for item in data:
+               items = item.strip().split()
+               for item in items: 
+                    if item in listData :
+                         continue
+                    elif item  not in listData:
+                         listData.append(item)
+     listData.sort()
+     print(listData)
+
+def assignment85() :
+     fname = input("Enter file name: ")
+     try:
+          data = open(fname)
+     except:
+          print("File Not Found")
+          quit()
+     count = 0
+     for line in data:
+          words = line.split()
+          if len(words)>1 and words[0] == "From":
+                    count+=1
+                    print(words[1])
+                    
+
+     print("There were", count, "lines in the file with From as the first word")
+
+# assignment85()
+
+
+      
