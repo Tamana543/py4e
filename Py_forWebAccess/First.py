ea8e9f4e2566b7a2e187707c990e403725754eb7
Tamana543 <tamanafarzami33@gmail.com>
2025-02-08 09:19:08 +0430

    File Updated

--- Py_forWebAccess/First.py
+++ Py_forWebAccess/First.py
@@ -1,8 +1,12 @@
+
+
+print("This is Tamana Farzami ,and I am happy to see you all here :)")
+
 print("This is Tamana Farzami ,and I am happy to see you all here :)")
 
 import re
 file = open('RegexCheetsheet')
 for line in file:
     line = line.rstrip()
-    if re.search('^//',line):
+    if re.search('^{',line):
         print(line)
