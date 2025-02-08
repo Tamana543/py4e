f00d67d2a10babb6c9cf566883bc8e97b7309ad9
Tamana Farzami <tamanafarzami33@gmail.com>
2025-02-07 20:45:02 -0800

    First Regex Practice Added

--- Py_forWebAccess/First.py
+++ Py_forWebAccess/First.py
@@ -1,1 +1,8 @@
 print("This is Tamana Farzami ,and I am happy to see you all here :)")
+
+import re
+file = open('RegexCheetsheet')
+for line in file:
+    line = line.rstrip()
+    if re.search('^//',line):
+        print(line)
