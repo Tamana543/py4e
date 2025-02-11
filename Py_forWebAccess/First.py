14b992c716c5428c9e8b59dfad6a16ed079d535c
Tamana543 <tamanafarzami33@gmail.com>
2025-02-11 09:26:44 +0430

    file Udpdated

--- Py_forWebAccess/First.py
+++ Py_forWebAccess/First.py
@@ -28,7 +28,7 @@ def assissmentData():
     print(item_sum)
 
 # assissmentData()
-
+#  Get the Socket and use it for extracting websites data 😉
 import socket 
 def gettingHTTPEng():
     mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -42,7 +42,43 @@ def gettingHTTPEng():
             break
         print(data.decode())
     mysock.close() 
-gettingHTTPEng()
+# gettingHTTPEng()
 
+# Or You can use this trick 
+import urllib.request, urllib.parse , urllib.error
+def gettingHTTPEng_() :
 
+    fhand = urllib.request.urlopen('https://en.wikipedia.org/wiki/Lorem_ipsum')
+    for line in fhand :
+     print(line.decode().strip())
+
+# gettingHTTPEng_()
+
+# To Count a WEbsite words 
+def web_words_counter() : 
+    fhand = urllib.request.urlopen('http://data.pr4e.org/intro-short.txt')
+    counts = dict()
+    for line in fhand:
+        words = line.decode().split()
+        for word in words :
+            counts[word] = counts.get(word,0) + 1
+    print(counts)
+# web_words_counter()
+
+from bs4 import BeautifulSoup
+import ssl
+# to ignore SSL certificate errors 
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+def linksOuter() :
+    file = input("File URL : ")
+    html = urllib.request.urlopen(file,context=ctx).read()
+    data = BeautifulSoup(html , 'html.parser')
+    tags = data('a')
+    for tag in tags :
+        print(tag.get('href' ,None))
+# linksOuter()
 
