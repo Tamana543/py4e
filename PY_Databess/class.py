80a30c928e54c61ad31f12542bdc3e7419ce5fca
Tamana543 <tamanafarzami33@gmail.com>
2025-04-26 13:59:47 +0430

    file Updated

--- PY_Databess/class.py
+++ PY_Databess/class.py
@@ -4,7 +4,12 @@ class partyCounter:
      def party(self):
           self.x = self.x+1
           print("So Far",self.x)
+     def __del__(self) : 
+          print("I am Destructed",self.x)
+
 an = partyCounter()
 an.party()
 an.party()
+an = 42
+print("an contains", an)
 print("Dir", dir(an))
