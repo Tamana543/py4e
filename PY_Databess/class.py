24321b6834fa0f10a0e7942206fd8361aaf08028
Tamana543 <tamanafarzami33@gmail.com>
2025-04-28 16:07:52 +0430

    inheritience by python (in classes) added

--- PY_Databess/class.py
+++ PY_Databess/class.py
@@ -13,3 +13,29 @@ an.party()
 an = 42
 print("an contains", an)
 print("Dir", dir(an))
+
+# Practicing inherietence 
+# main class
+class partyAnimal:
+     def __init__(self,name):
+          self.x = 0 
+          self.name = name
+          print(self.name, "constructed")
+
+     def party(self):
+          self.x = self.x +1
+          print(self.name, "party count",self.x)
+# The inherited class
+class FootballFan(partyAnimal):
+     def __init__(self, name):
+          super().__init__(name)
+          self.points = 0
+     def touchdown(self) : 
+          self.points = self.points + 7
+          self.party()
+          print(self.party, "Parties", self.points)
+s = partyAnimal("Sally")
+s.party()
+j = FootballFan("Jim")
+j.party()
+j.touchdown()
