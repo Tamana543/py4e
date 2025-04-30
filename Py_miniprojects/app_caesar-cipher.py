448e4ce9e38d7d666d00db05622d369d32e8dcd9
Tamana543 <tamanafarzami33@gmail.com>
2025-04-30 16:17:13 +0430

    first pro completed

--- Py_miniprojects/app_caesar-cipher.py
+++ Py_miniprojects/app_caesar-cipher.py
@@ -1,14 +1,17 @@
 
 text = input("Enter your message: ")
+num = int(input("Enter the cipher shift value (1..25)"))
 cipher = ''
 for char in text:
-    if not char.isalpha():
-        continue
-    char = char.upper()
-    code = ord(char) + 1
-    if code > ord('Z'):
-        code = ord('A')
-    cipher += chr(code)
-
-print(cipher)
-    
+          if num not in range(1,26) :
+               print("invalide shift value")
+          else:
+               if not char.isalpha():
+                    continue
+               char = char.upper()
+               code = ord(char) + num
+               if code > ord('Z'):
+                    code = ord('A')
+               cipher += chr(code)
+               print(cipher)
+     
