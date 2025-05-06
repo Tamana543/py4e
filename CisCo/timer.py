d582110f01d60b4c3a4a3bc50b12f26f383a3eab
Tamana543 <tamanafarzami33@gmail.com>
2025-05-06 15:02:47 +0430

    app completed

--- CisCo/timer.py
+++ CisCo/timer.py
@@ -22,3 +22,20 @@ class Timer :
                     self.__hours +=1
                     if self.__hours > 23 :
                          self.__hours = 0
+     def prev_second(self) :
+          self.__seconds -=1
+          if self.__seconds < 0 :
+               self.__seconds = 59
+               self.__minutes -= 1
+               if self.__minutes < 0 :
+                    self.__minutes = 59
+                    self.__hours -= 1
+                    if self.__hours < 0 :
+                         self.__hours = 23
+
+timer = Timer(23,59,59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
+print(timer)
