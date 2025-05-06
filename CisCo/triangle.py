779fdc40aed5892dab9e20d8f3febf7275127511
Tamana543 <tamanafarzami33@gmail.com>
2025-05-06 15:32:51 +0430

    min_app Completed

--- CisCo/triangle.py
+++ CisCo/triangle.py
@@ -9,4 +9,19 @@ class Point :
      def getY(self) :
           return self.__y
      def distance_from_xy(self,x , y) :
-          return 
+          return math.hypot(abs(self.__x - x),abs(self.__y - y))
+     def distance_from_point(self,point):
+          return self.distance_from_xy(point.getX(),point.getY())
+     
+class Triangle :
+     def __init__(self,vertice1,vertice2, vertice3):
+          self.__vertices = [vertice1,vertice2,vertice3]
+     def perimeter(self):
+          per = 0
+          for i in range(3):
+               per += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
+          return per
+
+triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
+
+print(triangle.perimeter())
