a146fc5af3a8b38a41f662657e3968a46d37a188
Tamana543 <tamanafarzami33@gmail.com>
2025-05-06 15:11:35 +0430

    app completed

--- CisCo/days_of-week.py
+++ CisCo/days_of-week.py
@@ -18,4 +18,8 @@ try :
      print(weekday)
      weekday.add_days(15)
      print(weekday)
-               
+     weekday.subtract_days(23) 
+     print(weekday)
+     weekday = Weeker("Monday")
+except WeekDayError :
+     print("Sorry, I can't serve your request today darling")         
