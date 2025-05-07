ec661e20847faa0413a487545c7b1835f76c420b
Tamana543 <tamanafarzami33@gmail.com>
2025-05-07 18:27:08 +0430

    file updated

--- CisCo/classes_more.py
+++ CisCo/classes_more.py
@@ -14,3 +14,27 @@ for cls1 in [Vehicle, LandVehicle, TrackedVehicle]:
     for cls2 in [Vehicle, LandVehicle, TrackedVehicle]:
         print(issubclass(cls1, cls2), end="\t")
     print()
+    
+print("................................")
+
+class Vehicle:
+    pass
+
+
+class LandVehicle(Vehicle):
+    pass
+
+
+class TrackedVehicle(LandVehicle):
+    pass
+
+
+my_vehicle = Vehicle()
+my_land_vehicle = LandVehicle()
+my_tracked_vehicle = TrackedVehicle()
+
+for obj in [my_vehicle, my_land_vehicle, my_tracked_vehicle]:
+    for cls in [Vehicle, LandVehicle, TrackedVehicle]:
+        print(isinstance(obj, cls), end="\t")
+    print()
+    
