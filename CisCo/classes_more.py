db660b4e936abf8096311786282e79afd51736ff
Tamana543 <tamanafarzami33@gmail.com>
2025-05-08 13:52:13 +0430

    file updated

--- CisCo/classes_more.py
+++ CisCo/classes_more.py
@@ -210,3 +210,31 @@ def print_exception_tree(thisclass, nest = 0):
 
 print_exception_tree(BaseException)
     
+def print_args(args):
+    lng = len(args)
+    if lng == 0:
+        print("")
+    elif lng == 1:
+        print(args[0])
+    else:
+        print(str(args))
+
+
+try:
+    raise Exception
+except Exception as e:
+    print(e, e.__str__(), sep=' : ' ,end=' : ')
+    print_args(e.args)
+
+try:
+    raise Exception("my exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
+
+try:
+    raise Exception("my", "exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
+	
