3440a3902f572509fa24097438422844659d0b29
Tamana543 <tamanafarzami33@gmail.com>
2025-05-08 13:47:23 +0430

    file updated

--- CisCo/classes_more.py
+++ CisCo/classes_more.py
@@ -127,26 +127,26 @@ print(obj.variable_1, obj.var_1, obj.fun_1())
 print(obj.variable_2, obj.var_2, obj.fun_2())
 print(obj.variable_3, obj.var_3, obj.fun_3())
     
-import time
+# import time
 
-class TrackedVehicle:
-    def control_track(left, stop):
-        pass
+# class TrackedVehicle:
+#     def control_track(left, stop):
+#         pass
 
-    def turn(left):
-        control_track(left, True)
-        time.sleep(0.25)
-        control_track(left, False)
+#     def turn(left):
+#         control_track(left, True)
+#         time.sleep(0.25)
+#         control_track(left, False)
 
 
-class WheeledVehicle:
-    def turn_front_wheels(left, on):
-        pass
+# class WheeledVehicle:
+#     def turn_front_wheels(left, on):
+#         pass
 
-    def turn(left):
-        turn_front_wheels(left, True)
-        time.sleep(0.25)
-        turn_front_wheels(left, False)
+#     def turn(left):
+#         turn_front_wheels(left, True)
+#         time.sleep(0.25)
+#         turn_front_wheels(left, False)
     
 
 
@@ -197,3 +197,16 @@ class GuardDog(Dog):
 rocky = SheepDog("Collie")
 luna = GuardDog("Dobermann")
  
+def print_exception_tree(thisclass, nest = 0):
+    if nest > 1:
+        print("   |" * (nest - 1), end="")
+    if nest > 0:
+        print("   +---", end="")
+
+    print(thisclass.__name__)
+
+    for subclass in thisclass.__subclasses__():
+        print_exception_tree(subclass, nest + 1)
+
+print_exception_tree(BaseException)
+    
