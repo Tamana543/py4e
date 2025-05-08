e2300da3a1e2e8fb026bc5be9e94f6fb2f38259e
Tamana543 <tamanafarzami33@gmail.com>
2025-05-08 13:36:12 +0430

    file updated

--- CisCo/classes_more.py
+++ CisCo/classes_more.py
@@ -174,4 +174,26 @@ object = Bottom()
 object.m_bottom()
 object.m_middle()
 object.m_top()
-    
+
+class Dog:
+    kennel = 0
+    def __init__(self, breed):
+        self.breed = breed
+        Dog.kennel += 1
+    def __str__(self):
+        return self.breed + " says: Woof!"
+ 
+ 
+class SheepDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Don't run away, Little Lamb!"
+ 
+ 
+class GuardDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Stay where you are, Mister Intruder!"
+ 
+ 
+rocky = SheepDog("Collie")
+luna = GuardDog("Dobermann")
+ 
