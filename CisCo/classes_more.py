9ef8221df8e54a2aa49ceaede57989f7a1b45411
Tamana543 <tamanafarzami33@gmail.com>
2025-05-09 15:19:02 +0430

    file updated

--- CisCo/classes_more.py
+++ CisCo/classes_more.py
@@ -379,3 +379,33 @@ filtered = list(filter(lambda x: x > 0 and x % 2 == 0, data))
 
 print(data)
 print(filtered)
+
+
+## Look for inner and outer func :  inner() may be invoked only from within outer(). We can say that inner() is outer()'s private tool – no other part of the code can access it.
+def outer(par):
+    loc = par
+
+    def inner():
+        return loc
+    return inner
+
+
+var = 1
+fun = outer(var)
+print(fun())
+
+#or 
+def make_closure(par):
+    loc = par
+
+    def power(p):
+        return p ** loc
+    return power
+
+
+fsqr = make_closure(2)
+fcub = make_closure(3)
+
+for i in range(5):
+    print(i, fsqr(i), fcub(i))
+    
