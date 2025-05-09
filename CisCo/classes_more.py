c6881ecb66b0ca4b4f5485c1081cdfe3c043c079
Tamana543 <tamanafarzami33@gmail.com>
2025-05-09 15:16:28 +0430

    Lambada function in map and filter added

--- CisCo/classes_more.py
+++ CisCo/classes_more.py
@@ -359,3 +359,23 @@ def print_function(args, fun):
         print('f(', x,')=', fun(x), sep='')
  
 print_function([x for x in range(-2, 3)], lambda x: 2 * x**2 - 4 * x + 2)
+
+#In map 
+
+list_1 = [x for x in range(5)]
+list_2 = list(map(lambda x: 2 ** x, list_1))
+print(list_2)
+
+for x in map(lambda x: x * x, list_2):
+    print(x, end=' ')
+print()
+
+# in filter 
+from random import seed, randint
+
+seed()
+data = [randint(-10,10) for x in range(5)]
+filtered = list(filter(lambda x: x > 0 and x % 2 == 0, data))
+
+print(data)
+print(filtered)
