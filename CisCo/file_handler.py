6b5eee4846f6f839e34b326c97c82603d920df49
Tamana543 <tamanafarzami33@gmail.com>
2025-05-10 10:19:22 +0430

    file updated

--- CisCo/file_handler.py
+++ CisCo/file_handler.py
@@ -18,4 +18,33 @@ try :
     s.close()
 except Exception as exc :
      print("The file could not be opened:", strerror(exc.errno)) # the sterror expects just one argument – an error number
-    
+
+try:
+    counter = 0
+    stream = open('text.txt', "rt")
+    char = stream.read(1)
+    while char != '':
+        print(char, end='')
+        counter += 1
+        char = stream.read(1)
+    stream.close()
+    print("\n\nCharacters in file:", counter)
+except IOError as e:
+    print("I/O error occurred: ", strerror(e.errno))
+print("next ")
+try:
+    ccnt = lcnt = 0
+    s = open('text.txt', 'rt')
+    lines = s.readlines(20)
+    while len(lines) != 0:
+        for line in lines:
+            lcnt += 1
+            for ch in line:
+                print(ch, end='')
+                ccnt += 1
+        lines = s.readlines(10)
+    s.close()
+    print("\n\nCharacters in file:", ccnt)
+    print("Lines in file:     ", lcnt)
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
