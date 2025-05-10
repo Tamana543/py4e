75987a9982321aaba66f0cfc241c2476af2a76ec
Tamana543 <tamanafarzami33@gmail.com>
2025-05-10 10:47:58 +0430

    Mini App :  sorted character frequency histogram  Added

--- CisCo/file_handler.py
+++ CisCo/file_handler.py
@@ -121,7 +121,8 @@ print(total,'byte(s) successfully written')
 src.close()
 dst.close()
 
-# Some mini apps 
+# Some mini apps ------
+
 #  character frequency histogram
 counters = {chr(ch) : 0 for ch in range(ord('a'),ord('z') + 1)}
 file_name = input("Enter the name of the file to analyze : ")
@@ -141,6 +142,25 @@ try :
             print(char,'->',c)
 except IOError as error :
     print("I/O error occirred :" , strerror(error.errno))
-    
-    
+
+# Sorted Character Frequency histogram 
+counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}
+file_name = input("Enter the name of the file to analyze: ")
+try:
+    file = open(file_name, "rt")
+    for line in file:
+        for char in line:
+            if char.isalpha():
+                counters[char.lower()] += 1
+    file.close()
+    file = open(file_name + '.hist', 'wt')
+    # use a lambda to access the directory's elements and set reverse to get a valid order.
+    for char in sorted(counters.keys(), key=lambda x: counters[x], reverse=True):
+        c = counters[char]
+        if c > 0:
+            file.write(char + ' -> ' + str(c) + '\n')
+    file.close()
+except IOError as e:
+    print("I/O error occurred: ", strerror(e.errno))
+
         
