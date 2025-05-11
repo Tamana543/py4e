c1291ab5e082736438ea5b99f108f01c0d2cee12
Tamana543 <tamanafarzami33@gmail.com>
2025-05-11 13:37:06 +0430

    file updated

--- CisCo/OS.py
+++ CisCo/OS.py
@@ -1,3 +1,15 @@
 import os
-# os.mkdir("First")
+# os.mkdir("my_first_directory")
 print(os.listdir)
+# os.makedirs("my_first_directory/my_second_directory")
+# os.chdir("my_first_directory")
+print(os.listdir())
+
+
+
+# os.makedirs("my_first_directory/my_second_directory")
+# os.chdir("my_first_directory")
+print(os.getcwd())
+# os.chdir("my_second_directory")
+print(os.getcwd())
+    
