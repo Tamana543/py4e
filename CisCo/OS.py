0e94f43057a679e8f5aec8aac9028d9f5d02fafe
Tamana543 <tamanafarzami33@gmail.com>
2025-05-11 13:51:51 +0430

    file updated

--- CisCo/OS.py
+++ CisCo/OS.py
@@ -23,3 +23,25 @@ print(os.listdir())
 
 returned_value = os.system("mkdir my_first_directory") #  returns the value returned by the shell after running the command given
 print(returned_value)
+
+# challenge : Write a function or method called find that takes two arguments called path and dir. The path argument should accept a relative or absolute path to a directory where the search should start, while the dir argument should be the name of a directory that you want to find in the given path. Your program should display the absolute paths if it finds a directory with the given name.
+# The directory search should be done recursively. This means that the search should also include all subdirectories in the given path
+
+class DirectorySearcher:
+    def find(self, path, dir):
+        try:
+            os.chdir(path)
+        except OSError:
+            # Doesn't process a file that isn't a directory.
+            return
+
+        current_dir = os.getcwd()
+        for entry in os.listdir("."):
+            if entry == dir:
+                print(os.getcwd() + "/" + dir)
+            self.find(current_dir + "/" + entry, dir)
+
+
+directory_searcher = DirectorySearcher()
+directory_searcher.find("./tree", "python")
+    
