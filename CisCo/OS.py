ed32257ffe75dcf4ecaf64b56fc95e43168f635d
Tamana543 <tamanafarzami33@gmail.com>
2025-05-11 13:45:35 +0430

    file updated

--- CisCo/OS.py
+++ CisCo/OS.py
@@ -12,4 +12,14 @@ print(os.listdir())
 print(os.getcwd())
 # os.chdir("my_second_directory")
 print(os.getcwd())
-    
+os.mkdir("my_first_directory")
+print(os.listdir())
+os.rmdir("my_first_directory") # to remove an empty directory 
+print(os.listdir())
+
+os.makedirs("my_first_directory/my_second_directory")
+os.removedirs("my_first_directory/my_second_directory") # to remove a directory which contains another one inside 
+print(os.listdir())
+
+returned_value = os.system("mkdir my_first_directory") #  returns the value returned by the shell after running the command given
+print(returned_value)
