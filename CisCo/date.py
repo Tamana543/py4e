6b196ae4daf516be7b53c429ce6d6ba2409b953b
Tamana543 <tamanafarzami33@gmail.com>
2025-05-11 14:21:35 +0430

    file and functionality updated

--- CisCo/date.py
+++ CisCo/date.py
@@ -1,4 +1,5 @@
 from datetime import date
+from datetime import time as t
 import time
 
 def firstPrac() : 
@@ -13,6 +14,23 @@ def firstPrac() :
      print(my_date)
      print(timestamp)
      print(dTimestamp)
+# firstPrac()
 
+def second() :
+     # to change the date exactly from date object : ( you can not do it directly as the date output is read-only)
+     d = date(2004,3,21)
+     d = d.replace(year=2005, month=4 , day=29)
+     print(d)
+     print(d.weekday())
+     print(d.isoweekday())
+# second()
 
-firstPrac()
+def timeObject() :
+     #time(hour, minute, second, microsecond, tzinfo, fold)
+     d = t(2,23,13,222)
+     print("Time:", d)
+     print("Hour:", d.hour)
+     print("Minute:", d.minute)
+     print("Second:", d.second)
+     print("Microsecond:", d.microsecond)
+timeObject()
