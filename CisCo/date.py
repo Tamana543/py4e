f5b23f9261f6a043beb31368b5909109b2bd26cc
Tamana543 <tamanafarzami33@gmail.com>
2025-05-12 16:19:58 +0430

    lab test added

--- CisCo/date.py
+++ CisCo/date.py
@@ -114,10 +114,28 @@ def first_calendar() :
      c = calendar.Calendar(calendar.SUNDAY)
      for weekday in c.iterweekdays():
       print(weekday, end=", ")
-first_calendar()
+# first_calendar()
 def delinder(data): 
     c = calendar.Calendar()
     for data in c.monthdays2calendar(2020, 12):
           print(data)
+    
 
-# LAb Test
+# Labe Test 
+class MyCalendar(calendar.Calendar):
+    def count_weekday_in_year(self, year, weekday):
+        current_month = 1
+        number_of_days = 0
+        while (current_month <= 12):
+            for data in self.monthdays2calendar(year, current_month):
+                if data[weekday][0] != 0:
+                    number_of_days = number_of_days + 1
+
+            current_month = current_month + 1
+        return number_of_days
+
+my_calendar = MyCalendar()
+number_of_days = my_calendar.count_weekday_in_year(2019, calendar.MONDAY)
+
+print(number_of_days)
+
